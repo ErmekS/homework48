a0e618463d5980121431cd4e672ffd2ceb3801b9
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-04 22:56:22 +0600

    Создал модель Product,Создал суперадмина. Добавил 5-7 товаров для примера с разными данными Создал фикстуру.

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_name', 'description', 'category', 'balance', 'price', 'created_time', 'updated_time']
+    list_display_links = ['product_name']
+    list_filter = ['category']
+    search_fields = ['product_name']
+    fields = ['product_name', 'category', 'balance', 'price', 'created_time', 'updated_time']
+    readonly_fields = ['created_time', 'updated_time']
+
+
+admin.site.register(Product, ProductAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -6,17 +6,17 @@ CATEGORY_CHOICES = [('other', 'Разное'), ('grocery', 'Бакалея'), ('
 
 class Product(models.Model):
     product_name = models.CharField(max_length=50, null=False, blank=False, default="No Product", verbose_name="Наименование товара")
-    entry = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Запись")
-    email = models.EmailField(max_length=20, null=False, blank=False, default="No Email", verbose_name="Email автора")
-
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание товара")
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name="Категория")
+    balance = models.IntegerField(verbose_name="Остаток")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_time = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
 
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
-                              verbose_name="Категория")
 
     def __str__(self):
-        return f"{self.id}. {self.entry}: {self.status} {self.author} {self.email}"
+        return f"{self.id}. {self.product_name}: {self.category} {self.balance} {self.price}"
 
     class Meta:
         db_table = "product"
