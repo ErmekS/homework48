7654457710c6284a020853f69742cfefb503785e
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-05 00:02:03 +0600

    создал базовый шаблон и главную страницу

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+# from webapp.forms import ProductForm
+from webapp.models import Product, CATEGORY_CHOICES
+
+
+def index_view(request):
+    products = Product.objects.order_by("category")
+    context = {"products": products}
+    return render(request, "index.html", context)
+
+
+def product_view(request, **kwargs):
+    pk = kwargs.get("pk")
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "product_view.html", {"product": product})
